4d451f6fabcac0392a21d87a76099bd6df557f97
Paulo Lacerda <paulodetasso@proton.me>
2023-06-11 18:15:26 -0300

    feature: set up nuxt js

---
+++ components/Dashboard/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <a href="/" id="logo">
+      <img src="/logo.png" alt="Paulo Lacerda Logo" />
+    </a>
+
+    <input type="text" placeholder="Search" />
+  
+    <nav id="menu">
+      <button>Light/Dark Mode</button>
+      <a href="/">Instagram</a>
+      <a href="/about">YouTube</a>
+      <a href="/contact">Twitter</a>
+    </nav>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <DashboardNavbar />
+
+  <main>
+    <div id="posts">
+      <div>
+      Post 1
+      </div>
+      <div>
+        Post 2
+      </div>
+      <div>
+        Post 3
+      </div>
+    </div>
+
+    <div id="courses">
+      Courses
+    </div>
+
+    <div id="skills">
+      Skills
+    </div>
+
+    <div id="projects">
+      Projects
+    </div>
+  </main>
+</template>
